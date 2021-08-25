dbdb2462636a5c1aaa8202ec1bc877e05dd5b334
Joe Pasqua <joe@noroomattheinn.org>
2021-08-25 14:07:40 -0700

    show "busy" icon while performing web activity

--- src/WebUIHelper.cpp
+++ src/WebUIHelper.cpp
@@ -15,6 +15,7 @@
 #include <WebUI.h>
 //                                  Local Includes
 #include "WTAppImpl.h"
+#include "gui/Theme.h"
 #include "gui/Display.h"
 #include "gui/ScreenMgr.h"
 //--------------- End:    Includes ---------------------------------------------
@@ -22,8 +23,39 @@
 
 // ----- BEGIN: WebUIHelper namespace
 namespace WebUIHelper {
+  static const char UpdatingSymbol = 'w';
   static const String   checkedOrNot[2] = {"", "checked='checked'"};
   ESPTemplateProcessor  *templateHandler;
+
+  void showUpdatingIcon() {
+    ScreenMgr::showUpdatingIcon(Theme::Color_WebRequest, UpdatingSymbol);
+  }
+
+  void hideUpdatingIcon() { ScreenMgr::hideUpdatingIcon(); }
+
+
+  void wrapWebAction(const char* actionName, std::function<void(void)> action) {
+    Log.trace(F("Handling %s"), actionName);
+    if (!WebUI::authenticationOK()) { return; }
+
+    showUpdatingIcon();
+    action();
+    hideUpdatingIcon();
+  }
+
+  void wrapWebPage(
+      const char* pageName, const char* htmlTemplate,
+      ESPTemplateProcessor::ProcessorCallback mapper)
+  {
+    Log.trace(F("Handling %s"), pageName);
+    if (!WebUI::authenticationOK()) { return; }
+
+    showUpdatingIcon();
+    WebUI::startPage();
+    templateHandler->send(htmlTemplate, mapper);
+    WebUI::finishPage();
+    hideUpdatingIcon();
+  }
 
   // ----- BEGIN: WebUIHelper::Internal
   namespace Internal {
@@ -48,155 +80,170 @@ namespace WebUIHelper {
   // ----- BEGIN: WebUIHelper::Endpoints
   namespace Endpoints {
     void setBrightness() {
-      if (!WebUI::authenticationOK()) { return; }
-      Log.trace(F("Handling /setBrightness"));
+      auto action = []() {
+        showUpdatingIcon();
+        uint8_t b = WebUI::arg(F("brightness")).toInt();
+        if (b <= 0 || b > 100) {  // NOTE: 0 is not an allowed value!
+          Log.warning(F("/setBrightness: %d is an unallowed brightness setting"), b);
+          WebUI::closeConnection(400, "Invalid Brightness: " + WebUI::arg(F("brightness")));
+        } else {
+          Display::setBrightness(b);
+          WebUI::closeConnection(200, F("Brightness Set"));
+        }
+      };
 
-      uint8_t b = WebUI::arg(F("brightness")).toInt();
-      if (b <= 0 || b > 100) {  // NOTE: 0 is not an allowed value!
-        Log.warning(F("/setBrightness: %d is an unallowed brightness setting"), b);
-        WebUI::closeConnection(400, "Invalid Brightness: " + WebUI::arg(F("brightness")));
-      } else {
-        Display::setBrightness(b);
-        WebUI::closeConnection(200, F("Brightness Set"));
-      }
+      wrapWebAction("/setBrightness", action);
     }
 
     void updateStatus() {
+      // NOTE: Don't display the updating icon. It will get done by the updating actions
       if (!WebUI::authenticationOK()) { return; }
       wtAppImpl->updateAllData();
       WebUI::redirectHome();
     }
 
     void updateWeatherConfig() {
-      if (!WebUI::authenticationOK()) { return; }
-      wtApp->settings->owmOptions.enabled = WebUI::hasArg(F("useOWM"));
-      wtApp->settings->uiOptions.useMetric = WebUI::hasArg(F("metric"));
-      wtApp->settings->owmOptions.key = WebUI::arg(F("openWeatherMapApiKey"));
-      wtApp->settings->owmOptions.cityID = WebUI::arg(F("cityID")).toInt();
-      wtApp->settings->owmOptions.language = WebUI::arg(F("language"));
-      wtApp->settings->owmOptions.nickname = WebUI::arg(F("nickname"));
+      auto action = []() {
+        wtApp->settings->owmOptions.enabled = WebUI::hasArg(F("useOWM"));
+        wtApp->settings->uiOptions.useMetric = WebUI::hasArg(F("metric"));
+        wtApp->settings->owmOptions.key = WebUI::arg(F("openWeatherMapApiKey"));
+        wtApp->settings->owmOptions.cityID = WebUI::arg(F("cityID")).toInt();
+        wtApp->settings->owmOptions.language = WebUI::arg(F("language"));
+        wtApp->settings->owmOptions.nickname = WebUI::arg(F("nickname"));
 
-      wtApp->settings->write();
-      // wtApp->settings->logSettings();
-      
-      wtAppImpl->weatherConfigMayHaveChanged();
-      WebUI::redirectHome();
+        wtApp->settings->write();
+        // wtApp->settings->logSettings();
+        
+        wtAppImpl->weatherConfigMayHaveChanged();
+
+        WebUI::redirectHome();
+      };
+
+      wrapWebAction("/updateWeatherConfig", action);
     }
 
     void updatePluginConfig() {
-      if (!WebUI::authenticationOK()) { return; }
+      auto action = []() {
+        String responseType = "text/plain";
+        if (!WebUI::hasArg(F("pID"))) {
+          uint8_t pID = WebUI::arg(F("pID")).toInt()-1;
+          Plugin *p = wtAppImpl->pluginMgr.getPlugin(pID);
 
-      String responseType = "text/plain";
+          if (p) {
+            if (WebUI::hasArg(F("plain"))) {
+              String settings = WebUI::arg("plain");
+              p->newSettings(settings);        
+              WebUI::sendStringContent(responseType, F("OK: settings updated"));
+            } else {
+              Log.warning(F("No payload supplied"));
+              WebUI::sendStringContent(responseType, F("ERR: No payload"));
+            }
+          } else {
+            Log.warning(F("Invalid plugin ID: %d"), pID);
+            WebUI::sendStringContent(responseType, F("ERR: Invalid plugin ID"));
+          }
+        } else {
+          Log.warning(F("No plugin ID was supplied"));
+          WebUI::sendStringContent(responseType, F("ERR: No plugin ID"));
+        }
+      };
 
-      if (!WebUI::hasArg(F("pID"))) {
-        Log.warning(F("No plugin ID was supplied"));
-        WebUI::sendStringContent(responseType, F("ERR: No plugin ID"));
-        return;
-      }
-
-      uint8_t pID = WebUI::arg(F("pID")).toInt()-1;
-      Plugin *p = wtAppImpl->pluginMgr.getPlugin(pID);
-      if (p == NULL) {
-        Log.warning(F("Invalid plugin ID: %d"), pID);
-        WebUI::sendStringContent(responseType, F("ERR: Invalid plugin ID"));
-        return;
-      }
-
-      if (!WebUI::hasArg(F("plain"))) {
-        Log.warning(F("No payload supplied"));
-        WebUI::sendStringContent(responseType, F("ERR: No payload"));
-        return;
-      }
-
-      String settings = WebUI::arg("plain");
-      p->newSettings(settings);        
-
-      WebUI::sendStringContent(responseType, F("OK: settings updated"));
+      wrapWebAction("/updateDisplayConfig", action);
     }
 
     void updateDisplayConfig() {
-      if (!WebUI::authenticationOK()) { return; }
+      auto action = []() {
+        wtApp->settings->uiOptions.schedule.active = WebUI::hasArg(F("scheduleEnabled"));
+        String t = WebUI::arg(F("morning"));
+        int separator = t.indexOf(":");
+        wtApp->settings->uiOptions.schedule.morning.hr = t.substring(0, separator).toInt();
+        wtApp->settings->uiOptions.schedule.morning.min = t.substring(separator+1).toInt();
+        wtApp->settings->uiOptions.schedule.morning.brightness = WebUI::arg(F("mBright")).toInt();
 
-      wtApp->settings->uiOptions.schedule.active = WebUI::hasArg(F("scheduleEnabled"));
-      String t = WebUI::arg(F("morning"));
-      int separator = t.indexOf(":");
-      wtApp->settings->uiOptions.schedule.morning.hr = t.substring(0, separator).toInt();
-      wtApp->settings->uiOptions.schedule.morning.min = t.substring(separator+1).toInt();
-      wtApp->settings->uiOptions.schedule.morning.brightness = WebUI::arg(F("mBright")).toInt();
+        t = WebUI::arg(F("evening"));
+        separator = t.indexOf(":");
+        wtApp->settings->uiOptions.schedule.evening.hr = t.substring(0, separator).toInt();
+        wtApp->settings->uiOptions.schedule.evening.min = t.substring(separator+1).toInt();
+        wtApp->settings->uiOptions.schedule.evening.brightness = WebUI::arg(F("eBright")).toInt();
 
-      t = WebUI::arg(F("evening"));
-      separator = t.indexOf(":");
-      wtApp->settings->uiOptions.schedule.evening.hr = t.substring(0, separator).toInt();
-      wtApp->settings->uiOptions.schedule.evening.min = t.substring(separator+1).toInt();
-      wtApp->settings->uiOptions.schedule.evening.brightness = WebUI::arg(F("eBright")).toInt();
+        wtApp->settings->uiOptions.use24Hour = WebUI::hasArg(F("is24hour"));
+        wtApp->settings->displayOptions.invertDisplay = WebUI::hasArg(F("invDisp"));
 
-      wtApp->settings->uiOptions.use24Hour = WebUI::hasArg(F("is24hour"));
-      wtApp->settings->displayOptions.invertDisplay = WebUI::hasArg(F("invDisp"));
+        wtApp->settings->write();
+        //wtApp->settings->logSettings();
 
-      wtApp->settings->write();
-      //wtApp->settings->logSettings();
-
-      WebUI::redirectHome();
+        WebUI::redirectHome();
+      };
+      
+      wrapWebAction("/updateDisplayConfig", action);
     }
   }   // ----- END: WebUIHelper::Endpoints
 
   // ----- BEGIN: WebUIHelper::Dev
   namespace Dev {
     void reboot() {
-      if (!WebUI::authenticationOK()) { return; }
-      wtAppImpl->askToReboot();
-      WebUI::redirectHome();
+      auto action = []() {
+        wtAppImpl->askToReboot();
+        WebUI::redirectHome();
+      };
+
+      wrapWebAction("/dev/reboot", action);
     }
 
     void forceScreen() {
-      Log.trace(F("Web Request: /dev/forceScreen"));
-      if (!WebUI::authenticationOK()) { return; }
-      String screen = WebUI::arg(F("screen"));
-      Log.trace(F("/dev/forceScreen?screen=%s"), screen.c_str());
-      if (screen.isEmpty()) return;
-      ScreenMgr::display(screen);
-      WebUI::redirectHome();
+      auto action = []() {
+        String screen = WebUI::arg(F("screen"));
+        Log.trace(F("/dev/forceScreen?screen=%s"), screen.c_str());
+        if (!screen.isEmpty()) {
+          if (screen.equalsIgnoreCase("home")) ScreenMgr::displayHomeScreen();
+          else ScreenMgr::display(screen);
+        }
+        WebUI::redirectHome();
+      };
+
+      wrapWebAction("/dev/forceScreen", action);
     }
 
     void yieldSettings() {
-      Log.trace(F("Web Request: /dev/settings"));
-      if (!WebUI::authenticationOK()) { return; }
-
-      DynamicJsonDocument *doc = (WebUI::hasArg("wt")) ? WebThing::settings.asJSON() :
-                                                         wtApp->settings->asJSON();
-      WebUI::sendJSONContent(doc);
-      doc->clear();
-      delete doc;
+      auto action = []() {
+        DynamicJsonDocument *doc = (WebUI::hasArg("wt")) ? WebThing::settings.asJSON() :
+                                                           wtApp->settings->asJSON();
+        WebUI::sendJSONContent(doc);
+        doc->clear();
+        delete doc;
+      };
+      wrapWebAction("/dev/settings", action);
     }
 
     void yieldScreenShot() {
-      Log.trace(F("Web Request: /dev/screenShot"));
-      if (!WebUI::authenticationOK()) { return; }
-
-      WebUI::sendArbitraryContent(
-          "image/bmp",
-          Display::getSizeOfScreenShotAsBMP(),
-          Display::streamScreenShotAsBMP);
+      auto action = []() {
+        WebUI::sendArbitraryContent(
+            "image/bmp",
+            Display::getSizeOfScreenShotAsBMP(),
+            Display::streamScreenShotAsBMP);
+      };
+      wrapWebAction("/dev/screenShot", action);
     }
 
     void enableDevMenu() {
-      if (!WebUI::authenticationOK()) { return; }
-      Log.trace("Web Request: /dev/enableDevMenu");
-
-      wtApp->settings->uiOptions.showDevMenu = WebUI::hasArg("showDevMenu");
-      wtApp->settings->write();
-
-      WebUI::redirectHome();
+      auto action = []() {
+        wtApp->settings->uiOptions.showDevMenu = WebUI::hasArg("showDevMenu");
+        wtApp->settings->write();
+        WebUI::redirectHome();
+      };
+      wrapWebAction("/dev/enableDevMenu", action);
     }
 
     void getDataBrokerValue() {
-      Log.trace(F("Web Request: /dev/data"));
-      if (!WebUI::authenticationOK()) { return; }
-      String key = "$" + WebUI::arg(F("key"));
-      String value;
-      DataBroker::map(key, value);
-      String result = key + ": " + value;
-      WebUI::sendStringContent("text", result);
+      auto action = []() {
+        String key = "$" + WebUI::arg(F("key"));
+        String value;
+        DataBroker::map(key, value);
+        String result = key + ": " + value;
+        WebUI::sendStringContent("text", result);
+      };
+
+      wrapWebAction("/dev/data", action);
     }
   }   // ----- END: WebUIHelper::Dev
 
@@ -204,51 +251,59 @@ namespace WebUIHelper {
   // ----- BEGIN: WebUIHelper::Default
   namespace Default {
     void devPage() {
-      Log.trace(F("Web Request: Handle Dev Configure"));
-      if (!WebUI::authenticationOK()) { return; }
+      auto action = []() {
+        auto mapper =[](String &key) -> String {
+          if (key.equals(F("HEAP"))) {
+            String heapStats;
+            heapStats.reserve(32);
+            DataBroker::map("$S.heap", heapStats);
+            return heapStats;
+          }
+          if (key.equals("SHOW_DEV_MENU")) return checkedOrNot[wtApp->settings->uiOptions.showDevMenu];
+          return WTBasics::EmptyString;
+        };
 
-      auto mapper =[](String &key) -> String {
-        ((void)key);
-        return WTBasics::EmptyString;
+        WebUI::startPage();
+        templateHandler->send("/wta/ConfigDev.html", mapper);
+        WebUI::finishPage();
       };
 
-      WebUI::startPage();
-      templateHandler->send("/wta/ConfigDev.html", mapper);
-      WebUI::finishPage();
+      wrapWebAction("/dev", action);
     }
 
     void updateDevConfig() {
-      if (!WebUI::authenticationOK()) { return; }
+      auto action = []() {
+        // Nothing to do by default...
+        WebUI::redirectHome();
+      };
 
-      WebUI::redirectHome();
+      wrapWebAction("/updateDevConfig", action);
     }
 
     void homePage() {
-      Log.trace(F("Web Request: Display Home Page"));
-      if (!WebUI::authenticationOK()) { return; }
+      auto action = []() {
+        auto mapper =[](String &key) -> String {
+          if (key.equals(F("CITYID"))) {
+            if (wtApp->settings->owmOptions.enabled) return String(wtApp->settings->owmOptions.cityID);
+            else return String("5380748");  // Palo Alto, CA, USA
+          }
+          if (key.equals(F("WEATHER_KEY"))) return wtApp->settings->owmOptions.key;
+          if (key.equals(F("UNITS"))) return wtApp->settings->uiOptions.useMetric ? "metric" : "imperial";
+          if (key.equals(F("BRIGHT"))) return String(Display::getBrightness());
+          return WTBasics::EmptyString;
+        };
 
-      auto mapper =[](String &key) -> String {
-        if (key.equals(F("CITYID"))) {
-          if (wtApp->settings->owmOptions.enabled) return String(wtApp->settings->owmOptions.cityID);
-          else return String("5380748");  // Palo Alto, CA, USA
-        }
-        if (key.equals(F("WEATHER_KEY"))) return wtApp->settings->owmOptions.key;
-        if (key.equals(F("UNITS"))) return wtApp->settings->uiOptions.useMetric ? "metric" : "imperial";
-        if (key.equals(F("BRIGHT"))) return String(Display::getBrightness());
-        return WTBasics::EmptyString;
+        WebUI::startPage();
+        templateHandler->send("/wta/HomePage.html", mapper);
+        WebUI::finishPage();
       };
 
-      WebUI::startPage();
-      templateHandler->send("/wta/HomePage.html", mapper);
-      WebUI::finishPage();
+      wrapWebAction("/", action);
     }
   }  // ----- END: WebUIHelper::Default
 
   namespace Pages {
     void presentWeatherConfig() {
-      Log.trace(F("Web Request: Handle Weather Configure"));
-      if (!WebUI::authenticationOK()) { return; }
-
       String langTarget = "SL" + wtApp->settings->owmOptions.language;
 
       auto mapper =[langTarget](String &key) -> String {
@@ -264,39 +319,34 @@ namespace WebUIHelper {
         return WTBasics::EmptyString;
       };
 
-      WebUI::startPage();
-      templateHandler->send("/wta/ConfigWeather.html", mapper);
-      WebUI::finishPage();
+      wrapWebPage("/presentWeatherConfig", "/wta/ConfigWeather.html", mapper);
     }
 
 
     void presentPluginConfig() {
-      Log.trace(F("Web Request: Handle Plugin Configure"));
-      if (!WebUI::authenticationOK()) { return; }
+      uint8_t count = wtAppImpl->pluginMgr.getPluginCount();
+      Plugin** plugins = wtAppImpl->pluginMgr.getPlugins();
 
-      auto mapper =[](String &key) -> String {
+      auto mapper =[count, plugins](String &key) -> String {
         if (key.startsWith("_P")) {
-          int i = (key.charAt(2) - '0');
-          key.remove(0, 4); // Get rid of the prefix; e.g. _P1_
-          Plugin *p = wtAppImpl->pluginMgr.getPlugin(i-1);
-          if (p == NULL) return WTBasics::EmptyString;
-          if (key.equals(F("IDX"))) return String(i);
-          if (key.equals(F("NAME"))) { return p->getName(); }
-          if (key.equals(F("FORM"))) { String v; p->getForm(v); return v; }
-          if (key.equals(F("VALS"))) { String v; p->getSettings(v); return v; }
+          int pluginIndex = (key.charAt(2) - '0') - 1;
+          if (pluginIndex < count) {
+            Plugin* p = plugins[pluginIndex];
+
+            key.remove(0, 4); // Get rid of the prefix; e.g. _P1_
+            if (key.equals(F("IDX"))) return String(pluginIndex+1);
+            if (key.equals(F("NAME"))) { return p->getName(); }
+            if (key.equals(F("FORM"))) { String v; p->getForm(v); return v; }
+            if (key.equals(F("VALS"))) { String v; p->getSettings(v); return v; }
+          }
         }
         return WTBasics::EmptyString;
       };
 
-      WebUI::startPage();
-      templateHandler->send("/wta/ConfigPlugins.html", mapper);
-      WebUI::finishPage();
+      wrapWebPage("/presentPluginConfig", "/wta/ConfigPlugins.html", mapper);
     }
 
     void presentDisplayConfig() {
-      Log.trace(F("Web Request: Handle Display Configure"));
-      if (!WebUI::authenticationOK()) { return; }
-
       auto mapper =[](String &key) -> String {
         if (key.equals(F("SCHED_ENABLED"))) return checkedOrNot[wtApp->settings->uiOptions.schedule.active];
         if (key.equals(F("MORN"))) return WebThing::formattedInterval(
@@ -311,9 +361,7 @@ namespace WebUIHelper {
         return WTBasics::EmptyString;
       };
 
-      WebUI::startPage();
-      templateHandler->send("/wta/ConfigDisplay.html", mapper);
-      WebUI::finishPage();
+      wrapWebPage("/presentPluginConfig", "/wta/ConfigDisplay.html", mapper);
     }
   }   // ----- END: WebUIHelper::Pages
 
@@ -341,6 +389,7 @@ namespace WebUIHelper {
     WebUI::registerHandler("/dev/forceScreen",        Dev::forceScreen);
     WebUI::registerHandler("/dev/enableDevMenu",      Dev::enableDevMenu);
     WebUI::registerHandler("/dev/data",               Dev::getDataBrokerValue);
+    WebUI::registerHandler("/dev/updateSettings",     Dev::updateDevConfig);
 
     templateHandler = WebUI::getTemplateHandler();
   }
--- src/WebUIHelper.h
+++ src/WebUIHelper.h
@@ -7,6 +7,16 @@ namespace WebUIHelper {
   extern ESPTemplateProcessor  *templateHandler;
 
   void init(String& customActions);
+
+  void showUpdatingIcon();
+  void hideUpdatingIcon();
+
+  void wrapWebAction(const char* actionName, std::function<void(void)> action);
+  void wrapWebPage(
+      const char* pageName, const char* htmlTemplate,
+      ESPTemplateProcessor::ProcessorCallback mapper);
+
+
   namespace Default {
     void homePage();
     void devPage();
