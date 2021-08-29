1e03c338a7fd0587c502b218aa53c398f064d191
Joe Pasqua <joe@noroomattheinn.org>
2021-08-29 07:33:30 -0700

    Implement mocking

--- src/clients/CoinbaseClient.h
+++ src/clients/CoinbaseClient.h
@@ -9,6 +9,13 @@
 
 #include <Arduino.h>
 
+// If you are in a low memory situation & can't afford the program or heap space
+// requirements of SSL, you can mock this service by #define-ing MockCoinbase
+// This is defined by defaul on ESP8266
+#if defined(ESP8266)
+  #define MockCoinbase
+#endif
+
 class CoinbaseClient {
 public:
   static bool getPrice(String coinID, String& currency, String& price);
