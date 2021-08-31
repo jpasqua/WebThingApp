edc43a1fc115f20e784f469e49ed87f0568f456e
Joe Pasqua <joe@noroomattheinn.org>
2021-08-31 11:08:03 -0700

    "Trick" to reduce heap fragmentation

--- src/plugins/Plugin.cpp
+++ src/plugins/Plugin.cpp
@@ -36,14 +36,28 @@ bool Plugin::init(const String& name, const String& piNamespace, const String& p
   _pluginDir = pluginDir;
   if (!typeSpecificInit()) return false;
 
-  _mapper = [&](const String& key, String &value) -> void {
+  _mapper = [this](const String& key, String &value) -> void {
     if (key.isEmpty()) return;
     if (key[0] == '$') { DataBroker::map(key, value); return; }
     else typeSpecificMapper(key, value);
   };
 
   // Create the FlexScreen UI
+
+  // The following call to malloc is actually there to *reduce* fragmentation! What happens normally
+  // is we allocate a huge chunk for the the JSON document, read it in, and then allocate the various
+  // FlexScreen objects. They are allocated "later" on the heap than the JSON document, so when we free
+  // it, there is a big hole left over (hence the fragmentation). By allocating the placeholder first,
+  // then allocating the JSON doc, then freeing the placeholder, we leave space "before" the JSON doc
+  // for the FlexScreen items to consume. Then when the JSON doc is freed, it doesnt leave a hole since
+  // it is freed from the end of the heap.
+  char* placeHolder = (char*)malloc(1024);
+  placeHolder[100] = 'C';   // Touch the memory or the compiler optimizes away the malloc
+
   DynamicJsonDocument* doc = PluginMgr::getDoc(_pluginDir + "/screen.json", MaxScreenDescriptorSize);
+
+  free(placeHolder);  // Free up the space for reuse by FlexScreen
+
   if (doc == NULL) return false;
   _flexScreen = ScreenMgr::createFlexScreen(*doc, getUIRefreshInterval(), _mapper);
   delete doc;
