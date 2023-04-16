34f3f5e0985700f71a23fc6da0dd3ec4b63bdfb2
Joe Pasqua <joe@noroomattheinn.org>
2023-04-15 18:53:46 -0700

    Add a faux signal strength icon

--- src/screens/matrix/WiFiScreen.h
+++ src/screens/matrix/WiFiScreen.h
@@ -18,13 +18,31 @@ public:
     uint16_t w = Display.mtx->width();
     uint16_t h = Display.mtx->height();
 
-    Display.drawStringInRegion(
-      "Wifi", Display.BuiltInFont_ID, Display.MC_Align,
-      0, h, w, h, w/2, h/2, 
-      Theme::Color_WHITE, Theme::Color_BLACK);
+    if (w >= 64) {
+      drawFauxStrengthIcon(0, h-1);
+      Display.drawStringInRegion(
+        "WiFi", Display.BuiltInFont_ID, Display.MC_Align,
+        0, h, w, h, w/2, h/2, 
+        Theme::Color_WHITE, Theme::Color_BLACK);
+    } else {
+      Display.drawStringInRegion(
+        "WiFi", Display.BuiltInFont_ID, Display.ML_Align,
+        0, h, w, h, 0, h/2, 
+        Theme::Color_WHITE, Theme::Color_BLACK);
+    }
+    drawFauxStrengthIcon(w-8, h-1);
+    Display.mtx->write();
   }
 
   virtual void processPeriodicActivity() { }
+
+  void drawFauxStrengthIcon(uint16_t x, uint16_t y) {
+    for (int bar = 1; bar < 5; bar++) {
+      int barSize = bar*2;
+      Display.mtx->drawFastVLine(x++, y-barSize, barSize, 1);
+      x++; // Skip a column
+    }
+  }
 };
 
 #endif
