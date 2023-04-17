c51079ab9a5e692bb3537599e4b3f12a94d4a80f
Joe Pasqua <joe@noroomattheinn.org>
2023-04-16 17:00:03 -0700

    Device-specific FlexScreen impls - no customization

---
+++ src/gui/devices/OLED/OLED_FlexScreen.h
@@ -0,0 +1,5 @@
+// The OLED class of display doesn't have any specializations for FlexScreen
+
+class FlexScreen : public BaseFlexScreen {
+	
+}
---
+++ src/gui/devices/Touch/Touch_FlexScreen.h
@@ -0,0 +1,5 @@
+// The Touch class of display doesn't have any specializations for FlexScreen
+
+class FlexScreen : public BaseFlexScreen {
+
+}
