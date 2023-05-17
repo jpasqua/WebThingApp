a89ce7804f07c0152db84d848008d677e17da2d1
Joe Pasqua <joe@noroomattheinn.org>
2023-05-16 19:17:57 -0700

    Add settings for which data to show

--- src/screens/matrix/WeatherScreen.cpp
+++ src/screens/matrix/WeatherScreen.cpp
@@ -12,6 +12,8 @@
 //                                  Third Party Libraries
 //                                  WebThing Includes
 #include <BPABasics.h>
+#include <Output.h>
+#include <ESP_FS.h>
 #include <WebThing.h>
 //                                  Local Includes
 #include "../../WTApp.h"
@@ -39,6 +41,18 @@
 WeatherScreen::WeatherScreen() {
   init();
   lastDT = 0;
+
+  if (!settings.fromJSON(WSSettings::settingsFilePath)) {
+    // No settings file, use defaults
+    settings.fields.emplace_back(WSSettings::Field("city", true));
+    settings.fields.emplace_back(WSSettings::Field("temp", true));
+    settings.fields.emplace_back(WSSettings::Field("wind", true));
+    settings.fields.emplace_back(WSSettings::Field("baro", false));
+    settings.fields.emplace_back(WSSettings::Field("humi", false));
+    settings.fields.emplace_back(WSSettings::Field("desc", true));
+    settings.fields.emplace_back(WSSettings::Field("ldesc", false));
+    settings.toJSON(WSSettings::settingsFilePath);
+  }
 }
 
 
@@ -59,18 +73,37 @@ void WeatherScreen::innerActivation() {
     return; // We haven't updated the weather so no need to update the text
   }
   String weatherText;
+  String delim = "";
 
-  String& city = wtApp->settings->owmOptions.nickname;
-  if (city.isEmpty()) { city = weather.location.city; }
+  for (WSSettings::Field f : settings.fields) {
+    if (!f.enabled) continue;
+    weatherText += delim;
+    delim = ", ";
+    if (f.id.equalsIgnoreCase("city")) {
+      String& city = wtApp->settings->owmOptions.nickname;
+      if (city.isEmpty()) { city = weather.location.city; }
+      weatherText += city;
+      delim = " ";
+    } else if (f.id.equalsIgnoreCase("temp")) {
+      weatherText += String(weather.readings.temp, 0);
+      weatherText += (wtApp->settings->uiOptions.useMetric ? "C" : "F");
+    } else if (f.id.equalsIgnoreCase("humi")) {
+      weatherText += String(weather.readings.humidity);
+      weatherText += '%';
+    } else if (f.id.equalsIgnoreCase("baro")) {
+      weatherText += String(weather.readings.pressure);
+      weatherText += Output::baroUnits();
+    } else if (f.id.equalsIgnoreCase("wind")) {
+      weatherText += String(weather.readings.windSpeed, 0);
+      weatherText += wtApp->owmClient->dirFromDeg(weather.readings.windDeg);
+    } else if (f.id.equalsIgnoreCase("desc")) {
+      weatherText += weather.description.basic;
+    } else if (f.id.equalsIgnoreCase("ldesc")) {
+      weatherText += weather.description.longer;
+    }
+  }
 
-  weatherText += city;
-  weatherText += " ";
-  weatherText += String(weather.readings.temp, 0);
-  weatherText += (wtApp->settings->uiOptions.useMetric ? "C" : "F");
-  weatherText += ' ';
-  weatherText += getTextForIcon(weather.description.icon);
   setText(weatherText);
-
   lastDT = weather.dt;
 }
 
@@ -87,6 +120,125 @@ String WeatherScreen::getTextForIcon(String& icon) {
   if (icon.startsWith("13")) { return "Snow"; }
   if (icon.startsWith("50")) { return "Haze"; }
   else return "";
+}
+
+/*------------------------------------------------------------------------------
+ *
+ * WSSettings Implementation
+ *
+ *----------------------------------------------------------------------------*/
+
+WSSettings::WSSettings() {
+  // Test
+  // fields.emplace_back(Field("city", true));
+  // fields.emplace_back(Field("temp", false));
+  // fields.emplace_back(Field("wind", true));
+}
+
+/* Available fields
+ fields: [
+  {id: "city", on: true},
+  {id: "temp", on: true},
+  {id: "humi", on: true}
+  {id: "baro", on: true},
+  {id: "wind", on: true}
+  {id: "desc", on: true},
+  {id: "ldesc", on: true},
+];
+*/
+
+
+bool WSSettings::fromJSON(const char* filePath) {
+  File f = ESP_FS::open(filePath, "r");
+  if (!f) {
+    Log.warning("No weather screen settings file was found: %s", filePath);
+    return false;
+  }
+
+  DynamicJsonDocument doc(MaxDocSize);
+
+  auto error = deserializeJson(doc, f);
+  if (error) {
+    Log.warning(F("Error parsing weather screen settings (%s): %s"), filePath, error.c_str());
+    return false;
+  }
+
+  fromJSON(doc);
+  if (fields.size() == 0) return false;
+  logSettings();
+  return true;
+}
+
+void WSSettings::fromJSON(const String &json) {
+  DynamicJsonDocument doc(MaxDocSize);
+  auto error = deserializeJson(doc, json);
+  if (error) {
+    Log.warning(F("Failed to parse weather screen field settings: %s"), error.c_str());
+    Log.warning(F("%s"), json.c_str());
+  }
+  fromJSON(doc);
+}
+
+void WSSettings::fromJSON(const JsonDocument &doc) {
+  fields.clear();
+  const JsonArrayConst& jsonFields = doc["fields"].as<JsonArray>();
+  Field field;
+  for (const auto& jsonField : jsonFields) {
+     field.enabled = jsonField["on"];
+     field.id = jsonField["id"].as<String>();
+     fields.push_back(field);
+  }
+}
+
+void WSSettings::toJSON(JsonDocument &doc) {
+  JsonArray jsonFields = doc.createNestedArray("fields");
+  for (Field& field : fields) {
+    JsonObject jsonField = jsonFields.createNestedObject();
+    jsonField["id"] = field.id;
+    jsonField["on"] = field.enabled;
+  }
+}
+
+
+void WSSettings::toJSON(const char* filePath) {
+  File settingsFile = ESP_FS::open(filePath, "w");
+  if (!settingsFile) {
+    Log.error(F("Failed to open file for writing: %s"), filePath);
+  }
+  Log.verbose("Writing weather screen settings to %s", filePath);
+  toJSON(settingsFile);
+  settingsFile.close();
+}
+
+void WSSettings::toJSON(Stream& s) {
+  // DynamicJsonDocument doc(MaxDocSize);
+  // toJSON(doc);
+  // serializeJson(doc, s);
+
+  s.println("{ \"fields\": [");
+  bool first = true;
+  for (Field& field : fields) {
+    if (!first) s.println(", ");
+    else first = false;
+    s.print("{");
+      s.print("\"id\":\""); s.print(field.id); s.print("\",");
+      s.print("\"on\":"); s.print(field.enabled ? "true" : "false");
+    s.print("}");
+  }
+  s.println("]}");
+}
+
+void WSSettings::toJSON(String &serialString) {
+  DynamicJsonDocument doc(MaxDocSize);
+  toJSON(doc);
+  serializeJson(doc, serialString);
+}
+
+void WSSettings::logSettings() {
+  Log.verbose(F("Weather Screen Field Selection"));
+  for (const Field& field : fields) {
+    Log.verbose(F("  %s, enabled: %T"), field.id.c_str(), field.enabled);
+  }
 }
 
 #endif
--- src/screens/matrix/WeatherScreen.h
+++ src/screens/matrix/WeatherScreen.h
@@ -12,6 +12,7 @@
 //--------------- Begin:  Includes ---------------------------------------------
 //                                  Core Libraries
 #include <Arduino.h>
+#include <vector>
 //                                  Third Party Libraries
 #include <ArduinoLog.h>
 //                                  WebThingApp Includes
@@ -19,13 +20,42 @@
 //                                  Local Includes
 //--------------- End:    Includes ---------------------------------------------
 
+class WSSettings {
+public:
+  static constexpr char* settingsFilePath = "/WSFields.json";
+  
+  struct Field {
+    Field() = default;
+    Field(const String& theID, bool on) : id(theID), enabled(on) { }
+    String id;
+    bool enabled;
+  };
+
+  WSSettings();
+
+  bool fromJSON(const char* filePath);
+  void fromJSON(const JsonDocument& doc);
+  void fromJSON(const String& json);
+  void toJSON(JsonDocument& doc);
+  void toJSON(Stream& s);
+  void toJSON(String& serialString);
+  void toJSON(const char* filePath);
+  void logSettings();
+
+  std::vector<Field> fields;
+
+private:
+  static constexpr size_t MaxDocSize = 512;
+};
+
 class WeatherScreen : public ScrollScreen {
 public:
   static String getTextForIcon(String& icon);
+  WSSettings settings;
 
   WeatherScreen();
   virtual void innerActivation() override;
-
+  void fieldsHaveBeenUpdated() { lastDT = UINT32_MAX; }
 
 private:
   uint32_t lastDT = UINT32_MAX;
